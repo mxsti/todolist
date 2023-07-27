50e41ba79fe7e711f17c7d95ee34ef16c44d79de
mxsty <lukastraue@gmail.com>
2023-07-27 12:03:46 +0200

    fix build error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google';
-
-
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string,
-        })
-    ],
-};
+import { authOptions } from "@/app/utils/authoptions";
 
 const handler = NextAuth(authOptions);
 
